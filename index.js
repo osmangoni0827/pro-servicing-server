bd18e155f01ec318b3d4d86a1ee60b0167e6364d
Mohammad Osman Goni <osmangoni0827@gmail.com>
2021-04-20 23:10:41 +0600

    update

--- index.js
+++ index.js
@@ -8,83 +8,112 @@ app.use(cors());
 app.use(bodyParser.json())
 const port = process.env.PORT || 4500
 const MongoClient = require('mongodb').MongoClient;
-const { static } = require('express');
+const ObjectId = require('mongodb').ObjectID;
 const { reset } = require('nodemon');
 console.log(process.env.DB_PASS, process.env.DB_USER)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.77ufn.mongodb.net/ProServicing?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const Ordercollection = client.db("ProServicing").collection("Orders");
-  const ServiceCollection=client.db("ProServicing").collection("Services");
-  const ReviewCollection=client.db("ProServicing").collection("Reviews");
-  const AdminCollection=client.db('ProServicing').collection("Admin")
+  const ServiceCollection = client.db("ProServicing").collection("Services");
+  const ReviewCollection = client.db("ProServicing").collection("Reviews");
+  const AdminCollection = client.db('ProServicing').collection("Admin")
   // perform actions on the collection object
 
-  app.post('/addOrder',(req,res)=>{
-      const Order=req.body;
-      Ordercollection.insertOne(Order)
-      .then(result=>{
-          res.send(result.insertedCount>0)
+  app.post('/addOrder', (req, res) => {
+    const Order = req.body;
+    Ordercollection.insertOne(Order)
+      .then(result => {
+        res.send(result.insertedCount > 0)
       })
   })
 
-app.get('/service',(req,res)=>{
-  ServiceCollection.find({})
-  .toArray((err,service)=>{
-    res.send(service);
-   
-  })
-})
+  app.get('/service', (req, res) => {
+    ServiceCollection.find({})
+      .toArray((err, service) => {
+        res.send(service);
 
-  app.post('/addService',(req,res)=>{
-    const Service=req.body;
+      })
+  })
+
+  app.post('/addService', (req, res) => {
+    const Service = req.body;
     ServiceCollection.insertOne(Service)
-    .then(result=>{
-      res.send(result.insertedCount>0)
-    })
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
   })
 
 
-  app.get('/review',(req,res)=>{
-    Ordercollection.find({})
-    .toArray((err,review)=>{
-      res.send(review);
-     if(err){
-       res.send(err)
-     }
-    })
+  app.get('/review', (req, res) => {
+    ReviewCollection.find({})
+      .toArray((err, review) => {
+        res.send(review);
+        if (err) {
+          res.send(err)
+        }
+      })
   })
 
-  app.post('/addReview',(req,res)=>{
-    const Review=req.body;
+  app.post('/addReview', (req, res) => {
+    const Review = req.body;
     ReviewCollection.insertOne(Review)
-    .then(result=>{
-      res.send(result.insertedCount>0)
-    })
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
   })
-  app.post('/orderList',(req,res)=>{
-    Ordercollection.find({})
-    .toArray((err,review)=>{
-      res.send(review);
-     if(err){
-       res.send(err)
-     }
-    })
+  app.post('/orderList', (req, res) => {
+    const email = req.body.email;
+    AdminCollection.find({ email: email })
+      .toArray((err, admin) => {
+        const filter = {}
+        if (admin.length === 0) {
+          filter.email = email;
+        }
+        Ordercollection.find(filter)
+          .toArray((err, documents) => {
+            res.send(documents);
+          })
+      })
   })
 
 
-  app.post('/addAdmin',(req,res)=>{
-    const Admin=req.body;
+
+
+
+  app.post('/addAdmin', (req, res) => {
+    const Admin = req.body;
     AdminCollection.insertOne(Admin)
-    .then(result=>{
-      res.send(result.insertedCount>0)
-    })
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
   })
+
+  app.get('/deleteService/:id', (req, res) => {
+    ServiceCollection.deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0)
+      })
+  })
+
+  app.patch('/update/:id', (req, res) => {
+    console.log(req.body.status);
+    Ordercollection.updateOne({ '_id': ObjectId(req.params.id) },
+      {
+        $set: { status: req.body.status }
+      })
+      .then(result => {
+        res.send(result.modifiedCount > 0);
+      })
+  })
+
+
+
 
   app.post('/isAdmin', (req, res) => {
     const email = req.body.email
 
-    doctorCollection.find({ email: email })
+    AdminCollection.find({ email: email })
       .toArray((err, doctor) => {
         res.send(doctor.length > 0)
       })
